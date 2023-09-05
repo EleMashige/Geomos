92ba5d1e1fcf4ced8a1b442baad858e5d39b0ac5
EleMashige <105087850+FatemRahimi@users.noreply.github.com>
2023-09-05 22:15:16 +0200

    added slide show

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 import Footer from './Footer';
+import Img1 from './photo_2018-07-10_14-44-18.jpg';
+import Img2 from './photo_2018-07-10_14-40-52.jpg';
+import './Home.css';
 
-function Home(){
-    return(
-<div>
-    <h1>Welcome</h1>
-    <p>This will be Clarence's Website</p>
+const imageArray = [Img1, Img2];
 
-    <Footer />
-</div>
-    )
-}
+const Home = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === imageArray.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="slideshow-container">
+      <Carousel
+        autoPlay={true}
+        showArrows={false}
+        showStatus={false}
+        showThumbs={false}
+        interval={5000}
+        selectedItem={currentImageIndex}
+        onChange={(index) => setCurrentImageIndex(index)}
+      >
+        {imageArray.map((image, index) => (
+          <div key={index} className="slide">
+            <img src={image} alt={`Photograph ${index + 1}`} />
+            {/* <p className="legend">{`Photograph ${index + 1}`}</p> */}
+          </div>
+        ))}
+      </Carousel>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
+
